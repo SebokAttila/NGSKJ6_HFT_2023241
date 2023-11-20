4a3d8b8d34d3f07043643233ff90d05a9bab147f
SebokAttila <attila.sebok20@gmail.com>
2023-11-20 17:45:41 +0100

    Barrrel class added.

---
+++ NGSKJ6_HFT_2023241_Models/Barrell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGSKJ6_HFT_2023241_Models
+{
+    public class Barrell
+    {
+
+    }
+}
