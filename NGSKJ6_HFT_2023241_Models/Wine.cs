5515c99964d4e5e8927f1cf3e80794458ebeb166
SebokAttila <attila.sebok20@gmail.com>
2023-11-20 17:43:23 +0100

    Wine class added.

---
+++ NGSKJ6_HFT_2023241_Models/Wine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGSKJ6_HFT_2023241_Models
+{
+    public class Wine
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WineId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Vintage { get; set; }
+        public int WineryId { get; set; }
+        [NotMapped]
+        public virtual Winery Winery { get; set; }
+    }
+}
