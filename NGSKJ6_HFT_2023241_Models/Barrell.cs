652799d1b14201ea6c1f029af49d87ea8dcabf2e
SebokAttila <attila.sebok20@gmail.com>
2023-11-20 17:57:38 +0100

    Parameters added to class

--- NGSKJ6_HFT_2023241_Models/Barrell.cs
+++ NGSKJ6_HFT_2023241_Models/Barrell.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,14 @@ namespace NGSKJ6_HFT_2023241_Models
 {
     public class Barrell
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BarrelId { get; set; }
+        public int Capacity { get; set; }
+        public string Material { get; set; }
+        public int NumberOfUses { get; set; }
+        public int WineId { get; set; }
+        [NotMapped]
+        public virtual Wine Wine { get; set; }
     }
 }
