f13aefafe438cf039a627abc27e4704e8d370853
SebokAttila <attila.sebok20@gmail.com>
2023-11-22 12:56:55 +0100

    Interface created and functions added..

---
+++ NGSKJ6_HFT_2023241_Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace NGSKJ6_HFT_2023241_Repository
+{
+    public interface IRepository <T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
