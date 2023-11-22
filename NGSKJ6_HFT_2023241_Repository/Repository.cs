afd47d754ccd35624ea72a8eec958b0d6bd86bf0
SebokAttila <attila.sebok20@gmail.com>
2023-11-22 13:10:37 +0100

    Interface implemented in class.

---
+++ NGSKJ6_HFT_2023241_Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGSKJ6_HFT_2023241_Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
