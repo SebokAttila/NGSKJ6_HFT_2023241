7546510a9ceb45c281c8f4702ef6bbce2cc6c956
SebokAttila <attila.sebok20@gmail.com>
2023-11-24 19:49:07 +0100

    Logic classes added and functions added to winelogic

---
+++ NGSKJ6_HFT_2023241_Logic/WineLogic.cs
@@ -0,0 +1,49 @@
+﻿using NGSKJ6_HFT_2023241_Models;
+using NGSKJ6_HFT_2023241_Repository;
+using System;
+using System.Linq;
+
+namespace NGSKJ6_HFT_2023241_Logic
+{
+    public class WineLogic
+    {
+        IRepository<Wine> repository;
+
+        public WineLogic(IRepository<Wine> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Wine item)
+        {
+            if (item.Name.Length < 0 || item.Name.Length > 99) throw new FormatException();
+
+            if (item.Amount <= 0)
+            {
+                throw new Exception();
+            }
+            this.repository.Create(item);
+        }
+        public void Delete(int id)
+        {
+            if (!repository.ReadAll().Select(p => p.WineId).Contains(id))
+            {
+                throw new FormatException();
+            }
+            this.repository.Delete(id);
+        }
+
+        public Wine Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+        public IQueryable<Wine> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+        public void Update(Wine wine)
+        {
+            this.repository.Update(wine);
+        }
+    }
+}
--- NGSKJ6_HFT_2023241_Repository/WineryDbContext.cs
+++ NGSKJ6_HFT_2023241_Repository/WineryDbContext.cs
@@ -13,5 +13,20 @@ namespace NGSKJ6_HFT_2023241_Repository
         public DbSet<Wine> Wines { get; set; }
         public DbSet<Winery> Wineries { get; set; }
         public DbSet<Barrell> Barrels { get; set; }
+
+        public WineryDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Wineries").UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
