8fe530cc9884656b739890c4cea6f5331e656d70
SebokAttila <attila.sebok20@gmail.com>
2023-11-29 22:04:54 +0100

    Entity framework added to db and web api created

--- NGSKJ6_HFT_2023241_Repository/WineryDbContext.cs
+++ NGSKJ6_HFT_2023241_Repository/WineryDbContext.cs
@@ -16,15 +16,35 @@ namespace NGSKJ6_HFT_2023241_Repository
 
         public WineryDbContext()
         {
-            Database.EnsureCreated();
+            this.Database.EnsureCreated();
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseInMemoryDatabase("Wineries").UseLazyLoadingProxies();
-            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseInMemoryDatabase("Wineries");
+
+
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Wine>(wine => wine
+            .HasOne(w => w.Winery)
+            .WithMany(winery => winery.Wines)
+            .HasForeignKey(w => w.WineryId)
+            .OnDelete(DeleteBehavior.Cascade));
+
+            modelBuilder.Entity<Barrell>(barrel => barrel
+            .HasOne(b => b.Wine)
+            .WithMany(w => w.Barells)
+            .HasForeignKey(b => b.WineId)
+            .OnDelete(DeleteBehavior.Cascade));
+            
+            
+            
+            
+            
+            
             base.OnModelCreating(modelBuilder);
         }
 
