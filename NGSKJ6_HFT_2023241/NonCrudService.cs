7b95f03d0f438e7691e1c5bf12701c012aa0627f
SebokAttila <attila.sebok20@gmail.com>
2023-12-05 19:05:31 +0100

    Menu started and service done

---
+++ NGSKJ6_HFT_2023241/NonCrudService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NGSKJ6_HFT_2023241_Models;
+
+namespace NGSKJ6_HFT_2023241_Client
+{
+    class NonCrudService
+    {
+        private RestService restService;
+
+        public NonCrudService(RestService restService)
+        {
+            this.restService = restService;
+        }
+        public void ListByVintage()
+        {
+            Console.WriteLine("Vintage:");
+            string vintage = Console.ReadLine();
+            var items = restService.Get<Wine>($"Stats/ListByVintage?vintage={vintage}");
+            foreach ( var item in items )
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+        public void BarrelsByMaterial()
+        {
+            Console.WriteLine("Material:");
+            string material = Console.ReadLine();
+            var barrels = restService.Get<Barrell>($"Stats/ListBarrelsByMaterial?material={material}");
+            foreach (var item in barrels)
+            {
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        public void BiggestBatch()
+        {
+            Console.WriteLine("Winery:");
+            string winery = Console.ReadLine();
+            var biggest = restService.Get<Wine>($"Stats/BiggestBatch?name={winery}");
+            Console.WriteLine(biggest);
+            Console.ReadLine();
+        }
+        public void WinesByWinery()
+        {
+            Console.WriteLine("Winery");
+            string winery = Console.ReadLine();
+            var wines = restService.Get<Wine>($"Stats/WinesByWinery?winery={winery}");
+            foreach (var item in wines)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+        public void BiggestBarrelInWinery()
+        {
+            Console.WriteLine("Winery");
+            string winery = Console.ReadLine();
+            var barrel = restService.Get<Barrell>($"Stats/BiggestBarrelInWinery?winery={winery}");
+            Console.WriteLine(barrel);
+            Console.ReadLine();
+        }
+    }
+}
--- NGSKJ6_HFT_2023241/Program.cs
+++ NGSKJ6_HFT_2023241/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using NGSKJ6_HFT_2023241_Client;
+using System;
 
 namespace NGSKJ6_HFT_2023241
 {
-    internal class Program
+    class Program
     {
+        static RestService RestService;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RestService = new RestService("http://localhost:21149/", "Winery");
+            CrudService crud = new CrudService(RestService);
+            NonCrud
         }
     }
 }
