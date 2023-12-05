4b228bb352923f9d19b83bd608fec49661d774d9
SebokAttila <attila.sebok20@gmail.com>
2023-12-05 18:26:02 +0100

    Logic interface updated with noncruds and noncrud controller done

--- NGSKJ6_HFT_2023241_Logic/Interfaces/IWineryLogic.cs
+++ NGSKJ6_HFT_2023241_Logic/Interfaces/IWineryLogic.cs
@@ -1,5 +1,6 @@
 ﻿using NGSKJ6_HFT_2023241_Models;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace NGSKJ6_HFT_2023241_Logic.Interfaces
 {
@@ -10,5 +11,12 @@ namespace NGSKJ6_HFT_2023241_Logic.Interfaces
         Winery Read(int id);
         IQueryable<Winery> ReadAll();
         void Update(Winery wine);
+        public IQueryable<Wine> ListByVintage(int vintage);
+        public IQueryable<Barrell> BarrellsByMaterial(string material);
+        public Wine BiggestBatch(string name);
+        public IQueryable<Wine> WinesByWinery(string winery);
+        public Barrell BiggestBarrelInWinery(string winery);
+
+
     }
 }
---
+++ NGSKJ6_HFT_2023341_Endpoint/Controllers/StatsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NGSKJ6_HFT_2023241_Logic.Interfaces;
+using NGSKJ6_HFT_2023241_Models;
+using System.Linq;
+
+namespace NGSKJ6_HFT_2023341_Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class StatsController : ControllerBase
+    {
+        IWineryLogic logic;
+
+        public StatsController(IWineryLogic logic)
+        {
+            this.logic = logic;
+        }
+        
+        [HttpGet]
+        public IQueryable<Wine> ListByVintage(int vintage)
+        {
+            return logic.ListByVintage(vintage);
+        }
+
+        [HttpGet]
+        public IQueryable<Barrell> BarrelsByMaterial(string material)
+        {
+            return logic.BarrellsByMaterial(material);
+        }
+
+        [HttpGet]
+        public Wine BiggestBatch([FromQuery] string name)
+        {
+            return logic.BiggestBatch(name);
+        }
+
+        [HttpGet]
+        public IQueryable<Wine> WinesByWinery([FromQuery] string winery)
+        {
+            return logic.WinesByWinery(winery);
+        }
+
+        [HttpGet]
+        public Barrell BiggestBarrelInWinery([FromQuery] string winery)
+        {
+            return logic.BiggestBarrelInWinery(winery);
+        }
+    }
+}
