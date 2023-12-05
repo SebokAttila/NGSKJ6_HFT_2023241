191cb321bd56e643f8399a0e3b292c1f4cd3f3a1
SebokAttila <attila.sebok20@gmail.com>
2023-12-05 16:27:22 +0100

    Startup services added

--- NGSKJ6_HFT_2023341_Endpoint/Startup.cs
+++ NGSKJ6_HFT_2023341_Endpoint/Startup.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NGSKJ6_HFT_2023241_Logic;
+using NGSKJ6_HFT_2023241_Logic.Interfaces;
+using NGSKJ6_HFT_2023241_Models;
+using NGSKJ6_HFT_2023241_Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +31,15 @@ namespace NGSKJ6_HFT_2023341_Endpoint
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<WineryDbContext>();
+            services.AddTransient<IRepository<Winery>,WineryRepository>();
+            services.AddTransient<IRepository<Wine>, WineRepository>();
+            services.AddTransient<IRepository<Barrell>, BarrellRepository>();
+
+            services.AddTransient<IWineryLogic, WineryLogic>();
+            services.AddTransient<IWineLogic, WineLogic>();
+            services.AddTransient<IBarrelLogic, BarrelLogic>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -43,6 +58,12 @@ namespace NGSKJ6_HFT_2023341_Endpoint
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NGSKJ6_HFT_2023341_Endpoint v1"));
             }
 
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
             app.UseRouting();
 
             app.UseAuthorization();
